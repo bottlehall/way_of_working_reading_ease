c1d72060c9341d6995b76e943d029e241e4c2731
Nicholas Robinson <nick.robinson@healthdatainsight.org.uk>
2024-01-05 12:52:44 +0000

    robocop'd some more...

--- src/complex-word.rb
+++ src/complex-word.rb
@@ -1,34 +1,42 @@
 #!/usr/bin/ruby
 # frozen_string_literal: true
 
-private
+# Estimates the number of syllables in the supplied word
+class ComplexWord
+  def initialize(word)
+    @word = word
+  end
 
-# module ComplexWord
-def remove_common_endings(word)
-  %w[es ing ed].map { |current| word = word.gsub("#{current} ", ' ') }
+  private
+
+  def remove_common_endings
+    # THIS IS NOT YET CORRECT!!!
+    %w[es ing ed].map { |current| @word = @word.gsub("#{current} ", ' ') }
+  end
+
+  def count_triple_vowels
+    triple_vowels = %w[iou eau auy]
+    syllables = triple_vowels.reduce(0) { |result, current| result + @word.scan(/(?=#{current})/).count }
+    triple_vowels.map { |current| @word = @word.gsub(current, '') }
+    syllables
+  end
+
+  def count_double_vowels
+    double_vowels = %w[ae ai ao au ay ea ee ei eo eu ey ia ie io iu oa oe oi oo ou oy ua ue ui uo uy]
+    syllables = double_vowels.reduce(0) { |result, current| result + @word.scan(/(?=#{current})/).count }
+    double_vowels.map { |current| @word = @word.gsub(current, '') }
+    syllables
+  end
+
+  public
+
+  def complex?
+    remove_common_endings
+    single_vowels = %w[a e i o u y]
+    syllables = count_triple_vowels + count_double_vowels + single_vowels.reduce(0) do |result, current|
+                                                              result + @word.scan(/(?=#{current})/).count
+                                                            end
+    # puts("#{@word} is #{syllables}")
+    syllables >= 3
+  end
 end
-
-def count_triple_vowels(word)
-  triple_vowels = %w[iou eau auy]
-  syllables = triple_vowels.reduce(0) { |result, current| result + word.scan(/(?=#{current})/).count }
-  triple_vowels.map { |current| word = word.gsub(current, '') }
-  syllables
-end
-
-def count_double_vowels(word)
-  double_vowels = %w[ae ai ao au ay ea ee ei eo eu ey ia ie io iu oa oe oi oo ou oy ua ue ui uo uy]
-  syllables = double_vowels.reduce(0) { |result, current| result + word.scan(/(?=#{current})/).count }
-  double_vowels.map { |current| word = word.gsub(current, '') }
-  syllables
-end
-
-public
-
-def complex?(word)
-  single_vowels = %w[a e i o u y]
-  syllables = count_triple_vowels(word) + count_double_vowels(word) + single_vowels.reduce(0) do |result, current|
-                                                                        result + word.scan(/(?=#{current})/).count
-                                                                      end
-  syllables >= 3
-end
-# end
