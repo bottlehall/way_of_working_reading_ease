c7329a7552bee4646d30587290b6e84e795dde1f
Nicholas Robinson <npr@bottlehall.co.uk>
2024-01-08 15:23:31 +0000

    reinstate legends

--- lib/fkre.rb
+++ lib/fkre.rb
@@ -12,6 +12,6 @@ class FleschKincaid
     }
 
     worst_score = scores.max_by { |score| score['score'] }['score'].round
-    print("FK_SCORE=#{worst_score}")
+    print("FK_SCORE=#{worst_score} FK_LEGEND=\"#{interpret_score(worst_score)}\"")
   end
 end
