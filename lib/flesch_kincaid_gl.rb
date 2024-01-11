7691e2651f9523718486e1beb82b7f9a74ab0573
Nicholas Robinson <npr@bottlehall.co.uk>
2024-01-11 11:39:04 +0000

    Prep for gem creation

--- lib/flesch_kincaid_gl.rb
+++ lib/flesch_kincaid_gl.rb
@@ -3,22 +3,22 @@
 
 # Wrapper class to link Thor CLI with the code for each file being indexed.
 class FleschKincaid
-  def initialize(filepaths, _badges = false, _threshold = 0)
+  def initialize(filepaths, badges = false, threshold = 0)
     scores = filepaths.map do |filepath|
       text = File.read(filepath)
       score = Odyssey.flesch_kincaid_gl(text, true)
-      if !_badges and score['score'] >= _threshold
-        puts( "#{filepath} #{score['score']}")
+      if !badges && score['score'] >= threshold
+        puts("#{filepath} #{score['score']}")
       else
         score
       end
     end
 
-    if _badges
-      worst_score = scores.max_by { |score| score['score'] }['score'].round
-      colour = worst_score > 25 ? 'red' : 'green'
-      print("FK_LEGEND=#{interpret_score(worst_score)}\nFK_COLOUR=#{colour}\n")
-    end
+    return unless badges
+
+    worst_score = scores.max_by { |score| score['score'] }['score'].round
+    colour = worst_score > 25 ? 'red' : 'green'
+    print("FK_LEGEND=#{interpret_score(worst_score)}\nFK_COLOUR=#{colour}\n")
   end
 end
 
